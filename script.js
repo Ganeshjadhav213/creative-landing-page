a6ee7ef576c8ba25b1286ffbc874cc67eabf351c
Ganeshjadhav213 <86249313+Ganeshjadhav213@users.noreply.github.com>
2021-11-02 16:51:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
